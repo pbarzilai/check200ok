d787ae3fff4ce50eb86136f7727781475660ae6a
pbarzilai <pba429@gmail.com>
2017-12-11 23:48:07 +0200

    rewriting script to use DB

--- check_sites_status.py
+++ check_sites_status.py
@@ -1,24 +1,26 @@
 #! /usr/bin/env python
 
 import time
+import psycopg2
+from config import config
 import pycurl
 import datetime
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-SITES = open("sites.txt").read().splitlines()
-SITESa = []
-
 def check_all():
-	f = open("sites.txt")
-	SITES = f.read().splitlines()
-	now = datetime.datetime.now()
-	for site in SITES:
+	params = config()
+	conn_sql = psycopg2.connect(**params)
+	cur = conn_sql.cursor()
+	cur.execute('SELECT hostname FROM sites_full order by id')
+	#cur.execute('SELECT hostname FROM sites_full where id = 26')
+	for hostname in cur:
+		now = datetime.datetime.now()
 		body = ""
 		try:
 			curl = pycurl.Curl()
-			curl.setopt(pycurl.URL, 'http://'+site)
+			curl.setopt(pycurl.URL, 'http://'+hostname)
 			curl.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36')
 			curl.setopt(pycurl.FOLLOWLOCATION, 1)
 			curl.setopt(pycurl.NOBODY, True)
@@ -26,57 +28,50 @@ def check_all():
 			curl.perform()
 
 			if curl.getinfo(pycurl.HTTP_CODE) >=400:
-				body += site + '\n'
+				body += hostname + '\n'
 				body += "    status code: %s" % curl.getinfo(pycurl.HTTP_CODE) + '\n'
 				body += 'Down since: ' + str(datetime.datetime.now()) + '\n\n'
 				startTime = datetime.datetime.now()
-				send_down(body, site)
-				SITESa.append(site)
-				#
-				f.close()
-				f = open("sites.txt", 'w')
-				SITES.pop(SITES.index(site))
-				f.write('\n'.join(SITES))
-				f.close()
+				#send_mail(body, hostname, ' is Down')
+				query = "UPDATE sites_full SET online = B'0' WHERE HOSTNAME = %s"
+				data = (hostname)
+				cur.execute(query, data)
+				conn_sql.commit()
 				time.sleep(5)
-				f = open("sites.txt")
-				SITES = f.read().splitlines()
-				#
 				body = ''
 		except pycurl.error, error:
 			errno, errstr = error
-			body += site + '\n'
+			body += hostname + '\n'
 			body += str('Error: ') + str(errstr) + '\n'
 			body += 'Down since: ' + str(datetime.datetime.now()) + '\n\n'
 			startTime = datetime.datetime.now()
-			send_down(body, site)
-			SITESa.append(site)
-			#
-			f.close()
-			f = open("sites.txt", 'w')
-			SITES.pop(SITES.index(site))
-			f.write('\n'.join(SITES))
-			f.close()
+			#send_mail(body, hostname, ' is Down')
+			query = "UPDATE sites_full SET online = B'0' WHERE HOSTNAME = %s"
+			data = (hostname)
+			cur.execute(query, data)
+			conn_sql.commit()
 			time.sleep(5)
-			f = open("sites.txt")
-			SITES = f.read().splitlines()
-			#
 			body = ''
 			continue
-	f.close()
-	fl = open("log.txt", 'a')
-	fl.write(now.strftime("%Y-%m-%d %H:%M") + ' Total Time Taken: ' + str(datetime.datetime.now() - now) + '\n')
-	fl.close()
+	cur.close()
+	conn_sql.close()
+	#f = open("log.txt", 'a')
+	#f.write(now.strftime("%Y-%m-%d %H:%M") + ' Total Time Taken: ' + str(datetime.datetime.now() - now) + '\n')
+	#f.close()
 	
 
 def check_again():
-	while len(SITESa) >0:
-		for site in SITESa:
+	params = config()
+	conn_sql = psycopg2.connect(**params)
+	cur = conn_sql.cursor()
+	cur.execute('SELECT hostname FROM sites_full ORDER BY id WHERE online = 0')
+	if cur >0:
+		for hostname in cur:
 			time.sleep(30)
 			bodyA = ""
 			try:
 				curl = pycurl.Curl()
-				curl.setopt(pycurl.URL, 'http://'+site)
+				curl.setopt(pycurl.URL, 'http://'+hostname)
 				curl.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36')
 				curl.setopt(pycurl.FOLLOWLOCATION, 1)
 				curl.setopt(pycurl.NOBODY, True)
@@ -84,48 +79,34 @@ def check_again():
 				curl.perform()
 
 				if curl.getinfo(pycurl.HTTP_CODE) <400:
-					bodyA += site + '\n'
+					bodyA += hostname + '\n'
 					bodyA += "    status code: %s" % curl.getinfo(pycurl.HTTP_CODE) + '\n'
 					bodyA += 'Up since: ' + str(datetime.datetime.now()) + '\n'
-					#bodyA += site + ' was down for: ' + str(datetime.datetime.now() - sT) + '\n'
-					send_up(bodyA, site)
-					SITESa.pop(SITESa.index(site))
-					#
-					#f.close()
-					f2 = open("sites.txt")
-					SITES = f2.read().splitlines()
-					f2.close()
-					f2 = open("sites.txt", 'w')
-					SITES.append(site)
-					f2.write('\n'.join(SITES))
-					f2.close()
-					#f = open("sites_short.txt")
-					#SITES = f.read().splitlines()
-					#
+					send_mail(bodyA, hostname, ' is now Up')
+					query = "UPDATE sites_full SET online = B'1' WHERE HOSTNAME = %s"
+					data = (hostname)
+					cur.execute(query, data)
+					conn_sql.commit()
 			except pycurl.error, error:
 				continue
+			cur.execute('SELECT hostname FROM sites_full ORDER BY id WHERE online = 0')
+			if cur >0:
+				continue
+			else:
+				break
+	#else:
+		#return
+	cur.close()
+	conn_sql.close()
 
-def send_up(b, s):
+def send_mail(b, s, msgS):
 	server = smtplib.SMTP('192.168.*.*', 25)
 	fromaddr = "*@*.com"
 	toaddr = "*@*.com"
 	msg = MIMEMultipart()
 	msg['From'] = fromaddr
 	msg['To'] = toaddr
-	msg['Subject'] = s + ' is now Up'
-	msg.attach(MIMEText(b, 'plain'))
-	server.ehlo()
-	text = msg.as_string()
-	server.sendmail(fromaddr, toaddr, text)
-
-def send_down(b, s):
-	server = smtplib.SMTP('192.168.*.*', 25)
-	fromaddr = "*@*.com"
-	toaddr = "*@*.com"
-	msg = MIMEMultipart()
-	msg['From'] = fromaddr
-	msg['To'] = toaddr
-	msg['Subject'] = s + ' is Down'
+	msg['Subject'] = s + msgS
 	msg.attach(MIMEText(b, 'plain'))
 	server.ehlo()
 	text = msg.as_string()
@@ -134,6 +115,7 @@ def send_down(b, s):
 def main():
 	check_all()
 	check_again()
+	
 
 if __name__ == "__main__":
 	main()
