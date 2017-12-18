eae33b4e4e4631b2cf3148ba947cbfa533f4f16d
pbarzilai <pba429@gmail.com>
2017-12-18 21:11:38 +0200

    small fix

--- check_sites_again.py
+++ check_sites_again.py
@@ -11,6 +11,13 @@ from email.MIMEText import MIMEText
 import filelock
 import sys
 
+def convert_timedelta(duration):
+    days, seconds = duration.days, duration.seconds
+    hours = seconds // 3600 + (days * 24)
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 60)
+    return hours, minutes, seconds
+
 def stop_if_already_running():
 	lock = filelock.FileLock("lock.txt")
 	lock.timeout = 2
@@ -26,14 +33,13 @@ def check_again():
 	while True:	
 		try:
 			params = config()
-			time.sleep(30)
 			conn_sql = psycopg2.connect(**params)
 			cur = conn_sql.cursor()
-			cur.execute('SELECT hostname FROM sites_full_test2 WHERE online = False')
+			cur.execute('SELECT hostname FROM sites_full WHERE online = False')
 			row = cur.fetchone()
 			if row is None:
 				break
-			cur.execute('SELECT hostname FROM sites_full_test2 WHERE online = False')
+			cur.execute('SELECT hostname FROM sites_full WHERE online = False')
 			for hostname in cur:
 				#print hostname
 				time.sleep(1)
@@ -52,17 +58,19 @@ def check_again():
 					curl.perform()
 
 					if curl.getinfo(pycurl.HTTP_CODE) <400:
-						query = "SELECT down_since FROM sites_full_test2 WHERE HOSTNAME = %s"
+						query = "SELECT down_since FROM sites_full WHERE HOSTNAME = %s"
 						data = (hostname,)
 						cur.execute(query, data)
 						data = cur.fetchone()
 						downsince = data[0]
 						bodyA += hostname + '\n'
 						bodyA += "    status code: %s" % curl.getinfo(pycurl.HTTP_CODE) + '\n'
-						bodyA += 'Site was down for: ' + str(datetime.datetime.now() - downsince) + '\n'
+						a = datetime.datetime.now() - downsince
+						hours, minutes, seconds = convert_timedelta(a)
+						bodyA += 'Site was down for: ' + '{} hours, {} minutes, {} seconds.'.format(hours, minutes, seconds) + '\n'
 						send_mail(bodyA, hostname, mUP)
 						reset_down_since = None
-						query = "UPDATE sites_full_test2 SET online = %s, down_since = %s WHERE HOSTNAME = %s"
+						query = "UPDATE sites_full SET online = %s, down_since = %s WHERE HOSTNAME = %s"
 						data = ('True', reset_down_since, hostname)
 						cur.execute(query, data)
 						conn_sql.commit()
@@ -72,6 +80,7 @@ def check_again():
 			conn_sql.close()
 		except psycopg2.ProgrammingError:
 			continue
+		time.sleep(30)
 					
 	
 def send_mail(b, s, msgS):
