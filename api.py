043da0ce8033d94c781772126b632e4ea82867f7
pbarzilai <pba429@gmail.com>
2017-12-23 21:59:23 +0200

    api get/post

--- api.py
+++ api.py
@@ -1,32 +1,49 @@
 #! /usr/bin/env python
 
+import sys
 import psycopg2
 from config import config
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask import jsonify
+from flask import Flask, jsonify, request, json
 
 app = Flask(__name__)
-api = Api(app)
 
-class Sites(Resource):
-	def get(self):
-		params = config()
-		conn_sql = psycopg2.connect(**params)
-		cur = conn_sql.cursor()
-		query = "SELECT * FROM sites_full order by hostname asc"
-		data = ('true',)
+@app.route('/offline_sites')
+def get():
+	params = config()
+	conn_sql = psycopg2.connect(**params)
+	cur = conn_sql.cursor()
+	cur.execute("SELECT hostname, online FROM sites_full where online <> 'true' or online IS NULL order by hostname asc")
+	r = [dict((cur.description[i][0], value) \
+	for i, value in enumerate(row)) for row in cur.fetchall()]
+	cur.close()
+	conn_sql.close()
+	return jsonify(r)
+	
+@app.route('/add_new_site', methods=['POST'])
+def add_site():
+	userinput = request.get_json()
+	new_site = userinput.get("hostname")
+	params = config()
+	conn_sql = psycopg2.connect(**params)
+	cur = conn_sql.cursor()
+	query = "SELECT hostname FROM sites_full where hostname = %s"
+	data = (new_site,)
+	cur.execute(query, data)
+	if cur.rowcount <> 0:
+		return "Site already exists in Database"
+	else:
+		query = "insert into sites_full (hostname) values (%s)"
+		data = (new_site,)
 		cur.execute(query, data)
-		r = [dict((cur.description[i][0], value) \
-		for i, value in enumerate(row)) for row in cur.fetchall()]
-		cur.close()
-		conn_sql.close()
-		return jsonify(r)
-
-api.add_resource(Sites, '/sites') # Route_1
+		conn_sql.commit()
+		return "Site added successfully"
+	cur.close()
+	conn_sql.close()
+		
 
 if __name__ == '__main__':
 	app.run(port='5002')
+	
 	 
 	 
 	 
